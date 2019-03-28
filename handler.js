9c8c708fbfe3d84cd405d8e1c3424724fac843d5
anorneto <anornetoo@gmail.com>
2019-03-28 14:57:59 -0300

    hello serverless :smile:

---
+++ handler.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports.hello = (event, context, callback) => {
+
+  console.log(event.pathParameters.name);
+
+  const now = new Date();
+
+  const reply = `Hello ${event.pathParameters.name} , the time is ${now}`;
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: JSON.stringify(reply)
+    })
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
